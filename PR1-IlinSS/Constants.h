325b0152393fc8aa3cd196b442ef96fa902350b7
SIlin <sergeykrotow@gmail.com>
2026-06-04 16:15:29 +0700

    добавлено Constants.h

---
+++ PR1-IlinSS/Constants.h
@@ -0,0 +1,24 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#include <string>
+
+namespace Constants {
+    
+    const std::string SPEED_RU = "скорость";
+    const std::string DISTANCE_RU = "расстояние";
+    const std::string OWNER_RU = "владелец";
+
+   
+    const std::string SPEED_EN = "speed";
+    const std::string DISTANCE_EN = "distance";
+    const std::string OWNER_EN = "owner";
+
+    
+    const std::string OP_GT = ">";
+    const std::string OP_LT = "<";
+    const std::string OP_EQ = "=";
+    const std::string OP_EQ2 = "==";
+}
+
+#endif
